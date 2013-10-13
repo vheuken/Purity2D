33b0af496b0557589a3d271ec712c8fb2d0b4ecb
Vincent Heuken <vincent@vincentheuken.com>
2013-10-13 14:57:19 -0700

    implemented Tile::draw

--- src/tile.cpp
+++ src/tile.cpp
@@ -4,3 +4,8 @@ Purity::Tile::Tile(const Tmx::Tile* tile, b2World* world)
 {
 
 }
+
+void Purity::Tile::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    target.draw(mTileSprite);
+}
--- src/tile.h
+++ src/tile.h
@@ -9,7 +9,13 @@ namespace Purity
 {
     class Tile : public Object
     {
+    public:
         Tile(const Tmx::Tile* tile, b2World* world);
+
+    private:
+        sf::Sprite mTileSprite;
+
+        void draw(sf::RenderTarget &target, sf::RenderStates states) const;
     };
 }
 
