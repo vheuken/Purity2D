9eccafee4be0645c10c08ee0b83fb3c604702128
Vincent Heuken <vincent@vincentheuken.com>
2014-01-14 10:45:08 -0800

    Host now disconnects peers in destructor

--- src/framework/host.cpp
+++ src/framework/host.cpp
@@ -2,5 +2,9 @@
 
 Purity::Host::~Host()
 {
+    for (size_t i = 0; i < mHost->connectedPeers; ++i)
+    {
+        enet_peer_disconnect(&mHost->peers[i], 0);
+    }
     enet_host_destroy(mHost);
 }
