0c43cd9b27405d19f77884fdf3257823007e16cf
Vincent Heuken <me@vincentheuken.com>
2014-04-10 17:39:17 -0700

    changed shared_ptr to unique_ptr

--- src/framework/entity/entity.h
+++ src/framework/entity/entity.h
@@ -64,7 +64,7 @@ namespace Purity
         Rect mHitboxRect;
 
         const Texture* mTexture;
-        std::shared_ptr<SpriteSheet> mSpriteSheet;
+        std::unique_ptr<SpriteSheet> mSpriteSheet;
         unsigned short mAnimationFrame;
 
         float mWidthPixels;
