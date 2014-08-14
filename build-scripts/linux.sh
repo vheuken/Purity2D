192ac707c2929806a69089602c8466265b7abe34
Vincent Heuken <me@vincentheuken.com>
2014-08-14 04:40:32 -0700

    fixed typo

--- build-scripts/linux.sh
+++ build-scripts/linux.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # GCC 4.7
 sudo apt-get -qq update
 sudo apt-get -qq install g++-4.7
-'[[ $CXX == "g++" ]] && export CXX=g++-4.7 && export CC=gcc-4.7'
+[[ $CXX == "g++" ]] && export CXX=g++-4.7 && export CC=gcc-4.7
 
 sudo apt-get install libpthread-stubs0-dev libgl1-mesa-dev libglu1-mesa-dev libx11-dev libxrandr-dev
 
