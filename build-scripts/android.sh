67407794eca63828d372bb48d0918e9697fbb948
Vincent Heuken <me@vincentheuken.com>
2014-08-23 08:50:00 -0700

    android build script now sets up standalone toolcain

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -1,2 +1,6 @@
 wget http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2
 tar xf android-ndk32-r10-linux-x86_64.tar.bz2
+
+export ANDROID_NDK=`pwd`/android-ndk-r10
+
+./$ANDROID_NDK/build/core/setup-toolchain.sh
