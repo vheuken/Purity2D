a884fe7a8119526b516b1304ae7102f0c04d20b2
Vincent Heuken <me@vincentheuken.com>
2014-08-26 17:43:10 -0700

    now uses config file for pixels_per_meter

--- src/framework/entity/entity.h
+++ src/framework/entity/entity.h
@@ -14,6 +14,8 @@
 #include "../graphics/transformable.h"
 #include "../spritesheet.h"
 
+#include "../system/configuration.h"
+
 
 namespace luabind
 {
@@ -27,7 +29,7 @@ namespace Tmx
 
 namespace Purity
 {
-    const float32 PIXELS_PER_METER = 30.0f;
+    const float32 PIXELS_PER_METER = Configuration::getInstance()->getReal("physics", "pixels_per_meter", 30.0);
     const float32 DEFAULT_HITBOX_WIDTH_METERS = 0.5;
     const float32 DEFAULT_HITBOX_HEIGHT_METERS = 1.5;
 
