2132e3e65db08f7952d2be4c0ab9451cf4cfb10a
Vincent Heuken <me@vincentheuken.com>
2014-08-28 01:49:02 -0700

    updated android build script

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -4,3 +4,10 @@ tar xf android-ndk32-r10-linux-x86_64.tar.bz2
 export ANDROID_NDK=`pwd`/android-ndk-r10
 
 bash $ANDROID_NDK/build/tools/make-standalone-toolchain.sh
+
+mkdir build && cd build
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/android.toolchain.cmake \
+         -DANDROID_NATIVE_API_LEVEL=android-19
+		
+cmake --build . -- -j4
