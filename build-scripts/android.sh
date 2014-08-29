aaf822c3c9e035f19a44229c7f74d693b9edb418
swloper <swloper@users.noreply.github.com>
2014-08-29 08:38:37 -0700

    Update android.sh

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -4,39 +4,45 @@ headerFormat="\n\n\e[1;34m%s\e[0m\n\n"
 #Need header $
 #$header="printf $Headerformat %s"
 
+
 printf "$headerFormat" "Working in location: `pwd`"
 export BUILD_HOME=`pwd` &&\
          printf "Created \$BUILD_HOME at `pwd`\n"
+
 
 #Need generic libraries, the following only works on Ubuntu 12.04
 printf "$headerFormat" "Installing i386 architecture libraries"
 sudo apt-get -qqy update
 sudo apt-get -qqy install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
 
+
 printf "$headerFormat" "Installing core Android development packages"
 printf "Downloading and extracting Android NDK\n"
 curl --location http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 \
          | tar -jx &&\
          printf "Extracted Android NDK to `pwd`\n"
-
 printf "Downloading and extracting Android SDK\n"
 curl --location http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz \
          | tar -zx &&\
          printf "Extracted Android NDK to `pwd`\n"
 
+
 printf "$headerFormat" "Configuring build environment"
-export ANDROID_NDK=`pwd`/android-ndk-r10 &&\
+cd $BUILD_HOME
+export ANDROID_NDK=./android-ndk-r10 &&\
          printf "Created \$ANDROID_NDK at `pwd`/android-ndk-r10\n"
-export ANDROID_SDK=`pwd`/android-sdk-linux &&\
+export ANDROID_SDK=./android-sdk-linux &&\
          printf "Created \$ANDROID_SDK at `pwd`/android-sdk-linux\n"
 export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools &&\
          printf "Added \$ANDROID_SDK/tools and \$ANDROID_SDK/platform-tools to \$PATH\n"
+
 
 #Workaround to allow Android SDK update automation, hardcoded for Android API level 20
 printf "$headerFormat" "Updating Android SDK"
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui \
          --filter "tools, platform-tools, build-tools-20.0.0, android-20"
 printf "$headerFormat" "Finished updating Android SDK"
+
 
 printf "$headerFormat" "Building engine"
 bash $ANDROID_NDK/build/tools/make-standalone-toolchain.sh
@@ -48,6 +54,7 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/android.toolchain.cmake \
 		
 cmake --build . -- -j4 && cd $BUILD_HOME
 
+
 printf "$headerFormat" "Building APK"
 cd $BUILD_HOME
 android update project \
@@ -55,9 +62,28 @@ android update project \
 
 ant debug
 
+ant release
+
+#Needs to be refined.
+keytool -genkey -noprompt \
+         -alias alias_name \
+         -dname "CN=, OU=, O=, L=, S=, C=" \
+         -keystore keystore \
+         -storepass password \
+         -keypass password
+         -keyalg RSA \
+         -keysize 2048 \
+         -validity 10000
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
+         -keystore keystore ./bin/purity2d-build.apk alias_name
+
 printf "$headerFormat" "Validating build"
 cd $BUILD_HOME
 ls -la ./bin
 
+jarsigner -verify -verbose -certs ./bin/purity2d-build.apk
 
+printf "$headerFormat" "Validating build again"
+ls -la ./bin
 
