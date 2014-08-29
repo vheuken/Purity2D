63858ba46954917ff2d27cc02bb4ea619e621530
swloper <swloper@users.noreply.github.com>
2014-08-29 07:24:41 -0700

    Update android.sh

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -10,7 +10,7 @@ export BUILD_HOME=`pwd` &&\
 
 printf "$headerFormat" "Installing i386 architecture libraries"
 sudo apt-get -y update
-sudo apt-get -y install ia32-libs
+sudo apt-get -y install ia32-libs-multiarch ia32-libs
 sudo apt-get -y install libncurses5:i386 libstdc++6:i386 zlib1g:i386
 sudo apt-get -y install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
 
