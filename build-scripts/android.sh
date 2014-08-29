daf1870c43e3cf4c4893f7e8f38cb87fe5017df4
swloper <swloper@users.noreply.github.com>
2014-08-29 02:33:42 -0700

    Update android.sh

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -1,13 +1,13 @@
 printf "\n\nWorking in location:     `pwd`\n"
 
 printf "\n\nInstalling core Android development packages\n"
-wget http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 | tar -x -f &
-wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz | tar -x -z &
+wget http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 | tar xf &
+wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz | tar xzf &
 wait
 
 printf "\n\nConfiguring and updating Android build environment\n"
 export ANDROID_NDK=`pwd`/android-ndk-r10 \
-         ANDROID_SDK=`pwd`/android-sdk-linux \
+         ANDROID_SDK=`pwd`/android-sdk-linux/sdk \
          PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools
 #Workaround to allow Android SDK update automation
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui
