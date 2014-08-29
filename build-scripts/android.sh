846f1a3ca140cfa595086246f864fb267dc22658
swloper <swloper@users.noreply.github.com>
2014-08-29 07:57:52 -0700

    Update android.sh

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -1,13 +1,14 @@
 #Formatting
-#Header
-headerFormat="\n\n\e[1;34m %s \e[0m\n\n"
-#Need header
+#Header format
+headerFormat="\n\n\e[1;34m%s\e[0m\n\n"
+#Need header $
 #$header="printf $Headerformat %s"
 
 printf "$headerFormat" "Working in location: `pwd`"
 export BUILD_HOME=`pwd` &&\
          printf "Created \$BUILD_HOME at `pwd`\n"
 
+#Need generic libraries, the following only works on Ubuntu 12.04
 printf "$headerFormat" "Installing i386 architecture libraries"
 sudo apt-get -y update
 #sudo apt-get -y install libncurses5:i386 libstdc++6:i386 zlib1g:i386
@@ -34,7 +35,8 @@ export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools &&\
 
 #Workaround to allow Android SDK update automation
 printf "$headerFormat" "Updating Android SDK"
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui #--filter "android-20"
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui \
+         --filter "tools", "platform-tools", "build-tools-20.0.0", "android-20"
 printf "$headerFormat" "Finished updating Android SDK"
 
 printf "$headerFormat" "Building engine"
