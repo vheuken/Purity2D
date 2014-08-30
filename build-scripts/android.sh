c0eb56bc42fb3605be7161fad456b313bc6c9810
swloper <swloper@users.noreply.github.com>
2014-08-29 22:44:39 -0700

    Test run

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -1,13 +1,15 @@
 #Formatting
-#Header format
+#Prints string in blue with two lines of surrounding whitespace.
 headerFormat="\n\n\e[1;34m%s\e[0m\n\n"
+#Prints directory in cyan
+location="\n\n\e[1;36m`pwd`[0m\n\n"
 #Need header $
 #$header="printf $Headerformat %s"
 
 
 printf "$headerFormat" "Working in location: `pwd`"
 export BUILD_HOME=`pwd` &&\
-         printf "Created \$BUILD_HOME at `pwd`\n"
+         printf "Created \$BUILD_HOME at $location\n"
 
 
 #Need generic libraries, the following only works on Ubuntu 12.04
@@ -20,26 +22,26 @@ printf "$headerFormat" "Installing core Android development packages"
 printf "Downloading and extracting Android NDK\n"
 curl --location http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 \
          | tar -jx &&\
-         printf "Extracted Android NDK to `pwd`\n"
+         printf "Extracted Android NDK to $location\n"
 printf "Downloading and extracting Android SDK\n"
 curl --location http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz \
          | tar -zx &&\
-         printf "Extracted Android NDK to `pwd`\n"
+         printf "Extracted Android NDK to $location\n"
 
 
 printf "$headerFormat" "Configuring build environment"
 export ANDROID_NDK=`pwd`/android-ndk-r10 &&\
-         printf "Created \$ANDROID_NDK at `pwd`/android-ndk-r10\n"
+         printf "Created \$ANDROID_NDK at $location/android-ndk-r10\n"
 export ANDROID_SDK=`pwd`/android-sdk-linux &&\
-         printf "Created \$ANDROID_SDK at `pwd`/android-sdk-linux\n"
+         printf "Created \$ANDROID_SDK at $location/android-sdk-linux\n"
 export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools &&\
          printf "Added \$ANDROID_SDK/tools and \$ANDROID_SDK/platform-tools to \$PATH\n"
 
 
-#Workaround to allow Android SDK update automation
+#Workaround to allow Android SDK update automation, hardcoded for Android  API 20 (4.4W). 
 printf "$headerFormat" "Updating Android SDK"
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui
-#         --filter "platform-tools, build-tools-20.0.0, android-20, build-tools-19"
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui \
+         --filter "platform-tools, build-tools-20.0.0, android-20"
 printf "$headerFormat" "Finished updating Android SDK"
 
 
@@ -82,17 +84,17 @@ keytool -genkey -noprompt \
 
 
 printf "$headerFormat" "Signing APK"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
-         -keystore keystore -storepass password ./bin/purity2d-build-release-unsigned.apk alias_name &&\
-         mv ./bin/purity2d-build-release-unsigned.apk ./bin/purity2d-build-release-signed.apk &&\
-         printf "Signed APK\n"
+cp ./bin/purity2d-build-release-unsigned.apk ./bin/purity2d-build-release-signing.apk \
+         && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
+                 -keystore keystore -storepass password ./bin/purity2d-build-release-unsigned.apk alias_name \
+         && mv ./bin/purity2d-build-release-signing.apk ./bin/purity2d-build-release-signed.apk \
+         && printf "Signed APK\n"
 
-printf "$headerFormat" "Validating build"
+printf "$headerFormat" "Validating builds"
 cd $BUILD_HOME
-ls -la ./bin
+jarsigner -verify -certs ./bin/purity2d-build-release-signed.apk
 
-jarsigner -verify -verbose -certs ./bin/purity2d-build-release-signed.apk
+printf "$headerFormat" "Builds Available:"
+ls -la ./bin | grep *.apk
 
-printf "$headerFormat" "Validating build again"
-ls -la ./bin
 
