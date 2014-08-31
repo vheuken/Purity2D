a0901ee7b473156f867d2272f1b5fff8afa583a2
swloper <swloper@users.noreply.github.com>
2014-08-30 18:13:06 -0700

    Update android.sh

--- build-scripts/android.sh
+++ build-scripts/android.sh
@@ -4,14 +4,14 @@ clearFormat=`tput sgr0`
 #tcBlue="\e[1;34m"
 #tcCyan="\e[1;36m"
 #tcNormal="\e[0m"
-black=`tput setf 1`
-red=`tput setf 2`
-green=`tput setf 3`
-yellow=`tput setf 4`
-blue=`tput setf 5`
-magenta=`tput setf 6`
-cyan=`tput setf 7`
-white=`tput setf 8`
+black=`tput setaf 1`
+red=`tput setaf 2`
+green=`tput setaf 3`
+yellow=`tput setaf 4`
+blue=`tput setaf 5`
+magenta=`tput setaf 6`
+cyan=`tput setaf 7`
+white=`tput setaf 8`
 #Prints string in bold blue with many lines of surrounding whitespace.
 headerFormat="\n\n\n\n\n${bright}${cyan}%s${clearFormat}\n\n"
 #Prints string in blue with one line of surrounding whitespace
@@ -22,45 +22,45 @@ location="${cyan}`pwd`${clearFormat}"
 #$header="printf $Headerformat %s"
 
 
-printf "$headerFormat" "Working in `pwd`"
+printf "${headerFormat}" "Working in `pwd`"
 export BUILD_HOME=`pwd` &&\
-         printf "$messageFormat" "Created \$BUILD_HOME at `pwd`"
+         printf "${messageFormat}" "Created \$BUILD_HOME at `pwd`"
 
 
 #Need generic libraries, the following only works on Ubuntu 12.04
-printf "$headerFormat" "Installing i386 architecture libraries"
+printf "${headerFormat}" "Installing i386 architecture libraries"
 sudo apt-get -qqy update
 sudo apt-get -qqy install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
 
 
-printf "$headerFormat" "Installing core Android development packages"
-printf "$messageFormat" "Downloading and extracting Android NDK"
+printf "${headerFormat}" "Installing core Android development packages"
+printf "${messageFormat}" "Downloading and extracting Android NDK"
 curl --location http://dl.google.com/android/ndk/android-ndk32-r10-linux-x86_64.tar.bz2 \
          | tar -jx \
-         && printf "$messageFormat" "Extracted Android NDK to `pwd`"
+         && printf "${messageFormat}" "Extracted Android NDK to `pwd`"
 printf "$messageFormat" "Downloading and extracting Android SDK"
 curl --location http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz \
          | tar -zx \
-         && printf "$messageFormat" "Extracted Android NDK to `pwd`"
+         && printf "${messageFormat}" "Extracted Android NDK to `pwd`"
 
 
-printf "$headerFormat" "Configuring build environment"
+printf "${headerFormat}" "Configuring build environment"
 export ANDROID_NDK=`pwd`/android-ndk-r10 \
-         && printf "$messageFormat" "Created \$ANDROID_NDK at `pwd`/android-ndk-r10"
+         && printf "${messageFormat}" "Created \$ANDROID_NDK at `pwd`/android-ndk-r10"
 export ANDROID_SDK=`pwd`/android-sdk-linux \
-         && printf "$messageFormat" "Created \$ANDROID_SDK at `pwd`/android-sdk-linux"
+         && printf "${messageFormat}" "Created \$ANDROID_SDK at `pwd`/android-sdk-linux"
 export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools \
-         && printf "$messageFormat" "Added \$ANDROID_SDK/tools and \$ANDROID_SDK/platform-tools to \$PATH"
+         && printf "${messageFormat}" "Added \$ANDROID_SDK/tools and \$ANDROID_SDK/platform-tools to \$PATH"
 
 
 #Workaround to allow Android SDK update automation, hardcoded for Android  API 20 (4.4W). 
-printf "$headerFormat" "Updating Android SDK"
+printf "${headerFormat}" "Updating Android SDK"
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui \
          --filter "platform-tools, build-tools-20.0.0, android-20"
-printf "$messageFormat" "Finished updating Android SDK"
+printf "${messageFormat}" "Finished updating Android SDK"
 
 
-printf "$headerFormat" "Building binaries"
+printf "${headerFormat}" "Building binaries"
 bash $ANDROID_NDK/build/tools/make-standalone-toolchain.sh
 
 mkdir build && cd build
@@ -71,23 +71,23 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/android.toolchain.cmake \
 cmake --build . -- -j4 && cd $BUILD_HOME
 
 
-printf "$headerFormat" "Starting APK build"
+printf "${headerFormat}" "Starting APK build"
 cd $BUILD_HOME
 android update project \
          --name purity2d-build --path . --target "android-20"
 
 
-printf "$headerFormat" "Building debug APK"
+printf "${headerFormat}" "Building debug APK"
 ant debug
 #Clarifying semantics
 mv ./bin/purity2d-build-debug.apk ./bin/purity2d-build-debug-aligned.apk
 
-printf "$headerFormat" "Building release APK"
+printf "${headerFormat}" "Building release APK"
 ant release
 #Clarifying semantics
 mv ./bin/purity2d-build-release-unsigned.apk ./bin/purity2d-build-release-unsigned-unaligned.apk
 
-printf "$headerFormat" "Generating signature"
+printf "${headerFormat}" "Generating signature"
 #Needs to be refined.
 keytool -genkey -noprompt \
          -alias alias_name \
@@ -98,39 +98,39 @@ keytool -genkey -noprompt \
          -keyalg RSA \
          -keysize 2048 \
          -validity 10000 \
-&& printf "$messageFormat" "Generated signature"
+&& printf "${messageFormat}" "Generated signature"
 
 
-printf "$headerFormat" "Creating signed release APK"
+printf "${headerFormat}" "Creating signed release APK"
 cd $BUILD_HOME
 cp ./bin/purity2d-build-release-unsigned-unaligned.apk ./bin/signing.apk \
          && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
                  -keystore keystore -storepass password ./bin/signing.apk alias_name \
          && mv ./bin/signing.apk ./bin/purity2d-build-release-signed-unaligned.apk \
-         && printf "$messageFormat" "Signed APK"
+         && printf "${messageFormat}" "Signed APK"
 
-printf "$headerFormat" "Zipaligning APKs"
+printf "${headerFormat}" "Zipaligning APKs"
 #Workaround for zipalign, hardcoded for API 20 (4.4W)
 ls $ANDROID_SDK/build-tools/20.0.0
 export PATH=$PATH:$ANDROID_SDK/build-tools/20.0.0 \
-         && printf "$messageFormat" "Added \$ANDROID_SDK/build-tools/20.0.0 to \$PATH"
-printf "$messageFormat" "Aligning signed APK"
+         && printf "${messageFormat}" "Added \$ANDROID_SDK/build-tools/20.0.0 to \$PATH"
+printf "${messageFormat}" "Aligning signed APK"
 zipalign -v 4 ./bin/purity2d-build-release-signed-unaligned.apk ./bin/purity2d-build-release-signed-aligned.apk
-printf "$messageFormat" "Aligning unsigned APK"
+printf "${messageFormat}" "Aligning unsigned APK"
 zipalign -v 4 ./bin/purity2d-build-release-unsigned-unaligned.apk ./bin/purity2d-build-release-unsigned-aligned.apk
 
-printf "$headerFormat" "Validating APK build signatures"
+printf "${headerFormat}" "Validating APK build signatures"
 cd $BUILD_HOME
-printf "$messageFormat" "Verifying aligned APK"
+printf "${messageFormat}" "Verifying aligned APK"
 jarsigner -verify -certs ./bin/purity2d-build-release-signed-aligned.apk
-printf "$messageFormat" "Verifying unaligned APK"
+printf "${messageFormat}" "Verifying unaligned APK"
 jarsigner -verify -certs ./bin/purity2d-build-release-signed-unaligned.apk
 
-printf "$headerFormat" "Contents of `pwd`/bin"
+printf "${headerFormat}" "Contents of `pwd`/bin"
 cd $BUILD_HOME
 ls -l ./bin
 
-printf "$headerFormat" "Builds Available:"
+printf "${headerFormat}" "Builds Available:"
 cd $BUILD_HOME
 ls -l ./bin/*.apk
 
