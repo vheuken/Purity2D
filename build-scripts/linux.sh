72fb4b8aaa830622a465c926c8e01de959206adf
swloper <swloper@users.noreply.github.com>
2014-08-31 05:42:16 -0700

    Update linux.sh

--- build-scripts/linux.sh
+++ build-scripts/linux.sh
@@ -51,7 +51,7 @@ make -j4
 
 printf "${headerFormat}" "Building packages"
 cd ${BUILD_BIN}
-mkdir $purity2d-build \
+mkdir purity2d-build \
           && cp {*,${BUILD_ASSETS}/*} $purity2d-build
           && tar -czf purity2d-build.tgz purity2d-build \
           && printf "${messageFormat}" "Done"
