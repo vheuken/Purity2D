a77daa8da012ca1e8c67568111f06171613c42ec
swloper <swloper@users.noreply.github.com>
2014-08-31 03:44:54 -0700

    Update .update-gh-pages.sh

--- .update-gh-pages.sh
+++ .update-gh-pages.sh
@@ -1,26 +1,24 @@
-#From http://sleepycoders.blogspot.com/2013/03/sharing-travis-ci-generated-files.html
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
-
-  #copy data we're interested in to other place
-  cp -R coverage $HOME/coverage
-
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
-
-  #go into diractory and copy data we're interested in to that directory
-  cd gh-pages
-  cp -Rf $HOME/coverage/* .
-
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
-
-  echo -e "Done magic with coverage\n"
+#Modified script originally from http://sleepycoders.blogspot.com/2013/03/sharing-travis-ci-generated-files.html
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    printf "Starting gh-pages update\n"
+    
+    printf "Setting up Git\n"
+    cd ${TRAVIS_BUILD_DIR}/release
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis"
+    
+    printf "Connecting to gh-pages\n"
+    #Currently hardcoded for swloper/Purity2D. 
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/swloper/Purity2D.git  gh-pages > /dev/null
+    
+    printf "Copying new files\n"
+    cd gh-pages
+    cp -Rf ${TRAVIS_BUILD_DIR}/release/* .
+    
+    printf "Committing and pushing files\n"
+    git add -f .
+    git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+    
+    printf "Done\n"
 fi
