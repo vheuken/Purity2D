70c34d12f2ac2a3a862847e8eea55b87871f061d
swloper <swloper@users.noreply.github.com>
2014-08-31 23:12:58 -0700

    Test release

--- build-scripts/osx.sh
+++ build-scripts/osx.sh
@@ -34,14 +34,38 @@ mkdir build && cd build
 cmake -G Xcode ..
 xcodebuild
 
-printf "${headerFormat}" "tests"
+
+printf "${headerFormat}" "Tests"
 printf "${messageFormat}" "Contents of ${BUILD_HOME}"
 ls -a ${BUILD_HOME}
 printf "${messageFormat}" "Contents of ${BUILD_BIN}"
 ls -a ${BUILD_BIN}
-printf "${messageFormat}" "Contents of ${BUILD_BIN}"
+printf "${messageFormat}" "Contents of ${BUILD_BIN}/Debug"
 ls -a ${BUILD_BIN}/Debug
 printf "${messageFormat}" "Contents of ${BUILD_ASSETS}"
 ls -a ${BUILD_ASSETS}
+printf "${messageFormat}" "Tests complete"
 
 
+printf "${headerFormat}" "Building packages"
+cd ${BUILD_BIN}
+printf "${messageFormat}" "Building debug package"
+mkdir purity2d-build 
+cp Debug/* purity2d-build
+cp -r ${BUILD_ASSETS}/* purity2d-build
+zip -r purity2d-build.zip purity2d-build \
+         && printf "${messageFormat}" "Done"
+
+printf "${headerFormat}" "Gathering final release files"
+cd ${BUILD_BIN}
+printf "${messageFormat}" "Contents of ${BUILD_BIN}"
+ls -a
+printf "${messageFormat}" "Exporting release (ZIP) files"
+cp *.zip ${BUILD_RELEASE} \
+         && printf "${messageFormat}" "Copied release files to ${BUILD_RELEASE}"
+
+
+printf "${headerFormat}" "Available releases:"
+cd ${BUILD_RELEASE}
+ls -1
+#ls | cat
