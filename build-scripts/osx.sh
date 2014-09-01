a7f349cd6b4102b4088892a63f95194fd059cc10
swloper <swloper@users.noreply.github.com>
2014-09-01 02:47:02 -0700

    Update osx.sh

--- build-scripts/osx.sh
+++ build-scripts/osx.sh
@@ -58,7 +58,7 @@ printf "${messageFormat}" "Debug message: copying assets"
 mkdir -p purity-2d-build/Purity-Engine.app/Contents/Resources
 ls -la purity-2d-build/Purity-Engine.app/Contents/Resources
 cd purity2d-build 
-cp -R ${BUILD_ASSETS}/* Purity-Engine.app #purity2d-build/Purity-Engine.app/Contents/Resources
+cp -R ${BUILD_ASSETS}/* Purity-Engine.app/Contents/ #purity2d-build/Purity-Engine.app/Contents/Resources
 cd ..
 printf "${messageFormat}" "Debug message: creating zip"
 zip -r purity2d-build.zip purity2d-build \
