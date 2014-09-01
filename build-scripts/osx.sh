2596efe62e271f5bc1ef5bcd18ebf9f270d92179
swloper <swloper@users.noreply.github.com>
2014-09-01 02:27:34 -0700

    Update osx.sh

--- build-scripts/osx.sh
+++ build-scripts/osx.sh
@@ -57,7 +57,8 @@ printf "${messageFormat}" "Debug message: copying assets"
 #OSX does not support the [-p   --parents] option in [cp]
 mkdir -p purity-2d-build/Purity-Engine.app/Contents/Resources
 ls -la purity-2d-build/Purity-Engine.app/Contents/Resources
-cp -R ${BUILD_ASSETS}/* purity2d-build #/Purity-Engine.app/Contents/Resources
+cd purity2d-build 
+cp -R ${BUILD_ASSETS}/* Purity-Engine.app #purity2d-build/Purity-Engine.app/Contents/Resources
 printf "${messageFormat}" "Debug message: creating zip"
 zip -r purity2d-build.zip purity2d-build \
          && printf "${messageFormat}" "Done"
