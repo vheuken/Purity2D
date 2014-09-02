ebe1bed39492d550e343fe3581af6652591786f4
swloper <swloper@users.noreply.github.com>
2014-09-02 05:15:57 -0700

    Update osx.sh

--- build-scripts/osx.sh
+++ build-scripts/osx.sh
@@ -36,6 +36,7 @@ xcodebuild \
          && xcodebuild -configuration Release
 
 printf "${headerFormat}" "Starting package build"
+
 printf "${headerFormat}" "Building debug package"
 cd ${BUILD_BIN}
 mkdir purity2d-build-debug
@@ -45,7 +46,8 @@ mkdir -p purity2d-build-debug/Purity-Engine.app/Contents/Resources
 cp -R ${BUILD_ASSETS}/* purity2d-build-debug/Purity-Engine.app/Contents/Resources/
 cd purity2d-build-debug
 zip --recurse-paths ../purity2d-build-debug.zip Purity-Engine.app
-printf "${headerFormat}" "Building relase package"
+
+printf "${headerFormat}" "Building relase package (ZIP)"
 cd ${BUILD_BIN}
 mkdir purity2d-build-release
 cp -R Release/* purity2d-build-release/
@@ -54,7 +56,63 @@ mkdir -p purity2d-build-release/Purity-Engine.app/Contents/Resources
 cp -R ${BUILD_ASSETS}/* purity2d-build-release/Purity-Engine.app/Contents/Resources/
 cd purity2d-build-release
 zip --recurse-paths ../purity2d-build-release.zip Purity-Engine.app
+
+
+
+
+printf "${headerFormat}" "Building relase package (PKG)"
+cd ${BUILD_BIN}
+cd purity2d-build-release
+
+printf "${messageFormat}" "Copying APP to `pwd`/app"
+mkdir -p app && cp -R Purity-Engine.app ./app/
+
+printf "${messageFormat}" "Running pkgbuild analyze"
+pwd
+ls -A
+pkgbuild --analyze --root ./app 'Purity-Engine.plist'
+cd app
+
+printf "${messageFormat}" "Making dummy files"
+pwd
+ls -A
+touch installerImage.png \
+         license.txt
+
+printf "${messageFormat}" "Running productbuild synthesize"
+pwd
+ls -A
+productbuild --synthesize \
+--package 'Purity-Engine.pkg' \
+Distribution.xml
+
+printf "${messageFormat}" "Running productbuild synthesize"
+pwd
+ls -A
+#mkdir resources
+#cp ./installerImage.png /resources
+#cp ./license.txt /resources
+ 
+#mkdir helper_scripts
+#cp ./postinstall 
+ 
+#cp $(SRC)/*.plist .
+#cp $(SRC)/Distribution.xml
+
+
+#chmod -R a+xr 'Purity-Engine.app'
+#chmod -R a+xr examples
+
+#printf "${messageFormat}" "Moving APP"
+#cp -Rfp 'Purity-Engine.app' app/'Purity-Engine.app'
+
+
 printf "${messageFormat}" "Done"
+
+
+
+
+
 
 printf "${headerFormat}" "Gathering final release files"
 cd ${BUILD_BIN}
