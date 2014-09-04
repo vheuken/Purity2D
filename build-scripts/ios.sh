dc6f14c34bdf7cc1c0accaa87dde62673d09e8f9
swloper <swloper@users.noreply.github.com>
2014-09-04 03:17:46 -0700

    Update ios.sh

--- build-scripts/ios.sh
+++ build-scripts/ios.sh
@@ -51,8 +51,8 @@ ls -A ./Purity2D.xcodeproj
 mkdir -p ./Purity2D.xcodeproj/xcshareddata/xcschemes
 cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity-Engine.xcscheme
 cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme
-printf "${messageFormat}" "Directory: `pwd`/Purity2D.xcodeproj/xcshareddata/xcschemes/"
-ls -A ./Purity2D.xcodeproj/xcshareddata/xcschemes/
+printf "${messageFormat}" "Directory: `pwd`/Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme"
+ls -A ./Purity2D.xcodeproj/xcshareddata/xcschemes
 
 ##printf "${messageFormat}" "Checking Ruby"
 ##rvm get head
@@ -142,7 +142,3 @@ printf "${headerFormat}" "Available releases:"
 cd ${BUILD_RELEASE}
 ls -1
 #ls | cat
-
-
-
-
