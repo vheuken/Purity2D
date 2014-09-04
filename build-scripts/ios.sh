964a425747d1373cba59975493f066acf86edcc2
swloper <swloper@users.noreply.github.com>
2014-09-04 05:39:05 -0700

    Update ios.sh

--- build-scripts/ios.sh
+++ build-scripts/ios.sh
@@ -29,97 +29,80 @@ mkdir -p release \
          && printf "${messageFormat}" "Created \${BUILD_RELEASE} at ${BUILD_RELEASE}"
 
 
-
-
-
-
-
-
 printf "${headerFormat}" "Building binaries"
 mkdir build
 cd build 
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iOS.cmake -DIOS_PLATFORM=SIMULATOR -G Xcode ..
 
-##printf "${headerFormat}" "Building debug binary"
-##xcodebuild
+printf "${headerFormat}" "Building debug binary"
+xcodebuild
 
 
-printf "${headerFormat}" "Building release binary"
-
-ls -A ./Purity2D.xcodeproj
-
-printf "${messageFormat}" "Copying XCSCHEME file"
-mkdir -p ./Purity2D.xcodeproj/xcshareddata/xcschemes
-cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity-Engine.xcscheme
-cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme
-printf "${messageFormat}" "Directory: `pwd`/Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme"
-ls -A ./Purity2D.xcodeproj/xcshareddata/xcschemes
-
-printf "${messageFormat}" "Listing XCode options"
-xcodebuild -list
-
-# Build archive
-printf "${messageFormat}" "Building archive"
-xcodebuild \
-         CODE_SIGN_IDENTITY="" \
-         CODE_SIGNING_REQUIRED=NO \
-         -scheme "Purity-Engine"
+#printf "${headerFormat}" "Building release binary"
+#
+#ls -A ./Purity2D.xcodeproj
+#
+#printf "${messageFormat}" "Copying XCSCHEME file"
+#mkdir -p ./Purity2D.xcodeproj/xcshareddata/xcschemes
+#cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity-Engine.xcscheme
+#cp ${BUILD_HOME}/.Purity-Engine.xcscheme ./Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme
+#printf "${messageFormat}" "Directory: `pwd`/Purity2D.xcodeproj/xcshareddata/xcschemes/Purity-Engine.xcscheme"
+#ls -A ./Purity2D.xcodeproj/xcshareddata/xcschemes
+#
+#printf "${messageFormat}" "Listing XCode options"
+#xcodebuild -list
+#
+## Build archive
+#printf "${messageFormat}" "Building archive"
+#xcodebuild \
+#         CODE_SIGN_IDENTITY="" \
+#         CODE_SIGNING_REQUIRED=NO \
+#         -scheme "Purity-Engine"
+##         -exportArchive \
+##         -archivePath Purity-Engine.xcarchive \
+##         -exportPath Purity-Engine.xcarchive
+##         -target Purity-Engine \
+##         -configuration Debug \
+#pwd
+#ls -A
+#
+## Export it to an IPA
+#printf "${messageFormat}" "Creating package"
+#xcodebuild \
 #         -exportArchive \
+#         -exportFormat ipa \
 #         -archivePath Purity-Engine.xcarchive \
-#         -exportPath Purity-Engine.xcarchive
-#         -target Purity-Engine \
-#         -configuration Debug \
-pwd
-ls -A
+#         -exportPath Purity-Engine.ipa \
+#         CODE_SIGN_IDENTITY="" \
+#         CODE_SIGNING_REQUIRED=NO \
+#         -alltargets \
+#         -configuration Debug
+#
+#printf "${headerFormat}" "Tests"
+#printf "${messageFormat}" "Contents of `pwd`"
+#pwd
+#ls -A
+#printf "${messageFormat}" "Contents of ${BUILD_HOME}"
+#cd ${BUILD_HOME}
+#pwd
+#ls -A
+#printf "${messageFormat}" "Contents of ${BUILD_BIN}"
+#cd ${BUILD_BIN}
+#pwd
+#ls -A
+#printf "${messageFormat}" "Contents of ${BUILD_BIN}/Debug"
+#cd ${BUILD_BIN}/Debug
+#pwd
+#ls -A
 
 
-# Export it to an IPA
-printf "${messageFormat}" "Creating package"
-xcodebuild \
-         -exportArchive \
-         -exportFormat ipa \
-         -archivePath Purity-Engine.xcarchive \
-         -exportPath Purity-Engine.ipa \
-         CODE_SIGN_IDENTITY="" \
-         CODE_SIGNING_REQUIRED=NO \
-         -alltargets \
-         -configuration Debug
-
-
-
-printf "${headerFormat}" "Tests"
-printf "${messageFormat}" "Contents of `pwd`"
-pwd
-ls -A
-printf "${messageFormat}" "Contents of ${BUILD_HOME}"
-cd ${BUILD_HOME}
-pwd
-ls -A
-printf "${messageFormat}" "Contents of ${BUILD_BIN}"
+printf "${headerFormat}" "Building debug package"
 cd ${BUILD_BIN}
-pwd
-ls -A
-printf "${messageFormat}" "Contents of ${BUILD_BIN}/Debug"
-cd ${BUILD_BIN}/Debug
-pwd
-ls -A
-
-
-
-
-
-
-
-
-
-
-##printf "${headerFormat}" "Building debug package"
-##cd ${BUILD_BIN}
-##mkdir purity2d-build-debug
-##cp -R Debug/* purity2d-build-debug/
-##cp -R ${BUILD_ASSETS}/* purity2d-build-debug/Purity-Engine.app
-##cd purity2d-build-debug
-##zip --recurse-paths ../purity2d-build-debug.zip Purity-Engine.app
+mkdir purity2d-build-debug
+cp -R Debug/* purity2d-build-debug/
+cp -R ${BUILD_ASSETS}/* purity2d-build-debug/Purity-Engine.app
+cd purity2d-build-debug
+zip --recurse-paths ../purity2d-build-debug.zip Purity-Engine.app
 
 
 printf "${headerFormat}" "Gathering final release files"
@@ -135,3 +118,7 @@ printf "${headerFormat}" "Available releases:"
 cd ${BUILD_RELEASE}
 ls -1
 #ls | cat
+
+
+
+
