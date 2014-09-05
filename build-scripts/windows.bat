d3767ceae2409074de5f35d5900b0b202112ad85
swloper <swloper@users.noreply.github.com>
2014-09-04 22:53:46 -0700

    Update windows.bat

--- build-scripts/windows.bat
+++ build-scripts/windows.bat
@@ -1,10 +1,14 @@
-echo Setting DXSDK path
+echo Adding DXSDK to PATH
 SET DXSDK_DIR=%ProgramFiles(x86)%\Windows Kits
+
+echo Showing PATH
+set
 
 :: Workaround for sh.exe in PATH
 echo Using workaround for sh.exe
 ren "C:/Program Files (x86)/Git/bin/sh.exe" shworkaround
 
+echo Building
 mkdir build
 cd build
 cmake -G "MinGW Makefiles" ..
