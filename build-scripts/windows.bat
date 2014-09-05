8da0dcb07de7be125c36e0cd8b417d2181654b7d
swloper <swloper@users.noreply.github.com>
2014-09-04 20:03:20 -0700

    Update windows.bat

--- build-scripts/windows.bat
+++ build-scripts/windows.bat
@@ -2,5 +2,7 @@ SET DXSDK_DIR=C:\Program Files x86\Windows Kits
 
 mkdir build
 cd build
-cmake ..
-MSBuild Purity2D.sln
+cmake -G "MinGW Makefiles" ..
+echo Is MinGW pathed?
+mingw32-make.exe all
+#MSBuild Purity2D.sln
