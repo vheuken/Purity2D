acbebafb118a6b8bf1e62d79a9c2055cd448f539
swloper <swloper@users.noreply.github.com>
2014-09-05 03:14:46 -0700

    Update windows.bat

--- build-scripts/windows.bat
+++ build-scripts/windows.bat
@@ -1,9 +1,14 @@
-echo Adding DXSDK to PATH
+echo Adding variables
 echo.
 SET DXSDK_DIR=%ProgramFiles(x86)%\Windows Kits
 SET(CMAKE_C_COMPILER C:/MinGW/bin/gcc)
 SET(CMAKE_CXX_COMPILER C:/MinGW/bin/g++)
 SET PATH=%PATH%;C:\MinGW\bin\
+echo.
+
+echo Using workaround for SDL bug
+echo.
+copy "C:\projects\purity2d\Bugfix_SDL_platform.h" "C:\projects\purity2d\extlibs\SDL2-2.0.3\include\SDL_platform.h"
 echo.
 
 :: Workaround for sh.exe in PATH
