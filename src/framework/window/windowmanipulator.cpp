297ee84b554708d5b4577345409ec0a472c650c1
Vincent Heuken <me@vincentheuken.com>
2014-09-10 18:47:31 -0700

    fixed issue where edges of window could be grabbed in bordered mode

--- src/framework/window/windowmanipulator.cpp
+++ src/framework/window/windowmanipulator.cpp
@@ -34,7 +34,7 @@ void Purity::WindowManipulator::setWindowFlags()
 {
     if (Mouse::isButtonPressed(Mouse::Left))
     {
-        if (mWindowResize == false && isMouseOnBorder())
+        if (mWindowResize == false && isMouseOnBorder() && mWindow->isBorderless())
         {
             mWindowResize = true;
             setBorderGrabbedFlags();
