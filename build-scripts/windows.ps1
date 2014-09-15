90d0455165c890379f0374e06ec04eeda80effa5
swloper <swloper@users.noreply.github.com>
2014-09-15 06:35:26 -0700

    Update windows.ps1

--- build-scripts/windows.ps1
+++ build-scripts/windows.ps1
@@ -1,21 +1,21 @@
 #Requires -Version 3
 
-Write-Host Creating "'${Env:BUILD_HOME}" in (Get-Location)
+Write-Host Creating "`${Env:BUILD_HOME}" in (Get-Location)
 ${Env:BUILD_HOME} = Get-Location
 
-Write-Host Creating "'${Env:BUILD_BIN}" in (Get-Location)\bin
+Write-Host Creating "`${Env:BUILD_BIN}" in (Get-Location)\bin
 New-Item "${Env:BUILD_HOME}\bin" `
          -ItemType directory `
          -Force `
          ; ${Env:BUILD_BIN} = "${Env:BUILD_HOME}\bin"
 
-Write-Host Creating "'${Env:BUILD_ASSETS}" in (Get-Location)\assets
+Write-Host Creating "`${Env:BUILD_ASSETS}" in (Get-Location)\assets
 New-Item "${Env:BUILD_HOME}\assets" `
          -ItemType directory `
          -Force `
          ; ${Env:BUILD_ASSETS} = "${Env:BUILD_HOME}\assets"
 
-Write-Host Creating "'${Env:BUILD_RELEASE}" in (Get-Location)\release
+Write-Host Creating "`${Env:BUILD_RELEASE}" in (Get-Location)\release
 New-Item "${Env:BUILD_HOME}\release" `
          -ItemType directory `
          -Force `
@@ -26,13 +26,18 @@ Invoke-Webrequest `
          -Uri "http://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe" `
          -Outfile "${Env:BUILD_HOME}\DXSDK_Jun10.exe"
 
-Write-Output "Installing DX SDK"
-.\DXSDK_Jun10.exe `
-         /S ` # Skips preinstallation checks
-         /O ` # Skips operating system checks
-         /U ` # Silent install
-         /P "${Env:SystemDrive}\DXSDK" # Default path is "${env:Programfiles}\DirectX SDK (June 2010)"
+Write-Host "Installing DX SDK"
+# /S skips prinstallation checks
+# /O skips operating system checks
+# /U does a silent install
+# /P sets the installation path, default "${env:Programfiles}\DirectX SDK (June 2010)"
+.\DXSDK_Jun10.exe /S /O /U /P "${Env:SystemDrive}\DXSDK"
 
+Set-Variable ${DXSDK_DIR} = ${Env:ProgramFiles}\DXSDK
+
+Write-Host "DXSDK_DIR at ${DXSDK_DIR}"
+
+Write-Host "Starting build"
 New-Item "${Env:BUILD_HOME}\build" `
          -ItemType directory `
          -Force
