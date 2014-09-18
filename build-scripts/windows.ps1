396ffe16e473afe363ed091a8675766258b2ffc9
swloper <swloper@users.noreply.github.com>
2014-09-17 19:42:41 -0700

    Update windows.ps1

--- build-scripts/windows.ps1
+++ build-scripts/windows.ps1
@@ -20,8 +20,7 @@ Invoke-WebRequest `
 7z `
          -t7z `
          "x32-4.8.1-release-posix-sjlj-rev5.7z" `
-         -o"${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\" `
-         | Find /V "Extracting " # 7z.exe does not have a quiet mode
+         -o"${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\"
 
 Write-Host "Adding MinGW to `${Env:Path}"
 ${Env:Path} = ${Env:Path} + ";${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\mingw32\bin"
