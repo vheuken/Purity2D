4cf71092c632ded018c8adbbea0d85ee3ce3272a
swloper <swloper@users.noreply.github.com>
2014-09-18 17:30:59 -0700

    Update windows.ps1

--- build-scripts/windows.ps1
+++ build-scripts/windows.ps1
@@ -102,7 +102,7 @@ Rename-Item `
          "C:\Program Files (x86)\Git\bin\shworkaround" `
          "sh.exe"
 
-sh.exe --login -i "${Env:APPVEYOR_BUILD_FOLDER}\build-scripts\ci\appveyor\update-gh-pages.sh" | Out-File test.txt
+sh.exe --login -i "${Env:APPVEYOR_BUILD_FOLDER}\build-scripts\ci\appveyor\update-gh-pages.sh"
 
 Get-Content test.txt
 
