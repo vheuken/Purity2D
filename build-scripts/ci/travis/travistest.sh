4b20ab4f65ab90dce15a8b61a48c501fb2e26ac3
swloper <swloper@users.noreply.github.com>
2014-09-22 21:27:26 -0700

    Update travistest.sh

--- build-scripts/ci/travis/travistest.sh
+++ build-scripts/ci/travis/travistest.sh
@@ -1,78 +1,118 @@
 printf "Expected Travis environment variables\n"
 printf "CI\n"
 printenv "CI"
+
 printf "TRAVIS\n"
 printenv "TAVIS"
+
 printf "CONTINUOUS_INTEGRATION\n"
 printenv "CONTINUOUS_INTEGRATION"
+
 printf "DEBIAN_FRONTEND\n"
 printenv "DEBIAN_FRONTEND"
+
 printf "HAS_JOSH_K_SEAL_OF_APPROVAL\n"
 printenv "HAS_JOSH_K_SEAL_OF_APPROVAL"
+
 printf "USER - Travis note: do not depend on this value\n"
 printenv "USER"
-printf "HOME\n"
+
+printf "HOME - Travis note: do not depend on this value\n"
 printenv "HOME"
+
 printf "LANG\n"
 printenv "LANG"
+
 printf "LC_ALL\n"
 printenv "LC_ALL"
+
 printf "RAILS_ENV\n"
 printenv "RAILS_ENV"
+
 printf "RACK_ENV\n"
 printenv "RACK_ENV"
+
 printf "MERB_ENV\n"
 printenv "MERB_ENV"
+
 printf "JRUBY_OPTS\n"
 printenv "JRUBY_OPTS"
+
 printf "TRAVIS_BRANCH\n"
-printenv "TRAVIS_BUILD_DIR"
+printenv "TRAVIS_BRANCH"
+
 printf "TRAVIS_BUILD_DIR\n"
-printenv "TRAVIS_BUILD_ID"
+printenv "TRAVIS_BUILD_DIR"
+
 printf "TRAVIS_BUILD_ID\n"
-printenv "TRAVIS_BUILD_NUMBER"
+printenv "TRAVIS_BUILD_ID"
+
 printf "TRAVIS_BUILD_NUMBER\n"
+printenv "TRAVIS_BUILD_NUMBER"
+
+printf "TRAVIS_COMMIT\n"
 printenv "TRAVIS_COMMIT"
+
 printf "TRAVIS_COMMIT_RANGE\n"
 printenv "TRAVIS_COMMIT_RANGE"
+
 printf "TRAVIS_JOB_ID\n"
 printenv "TRAVIS_JOB_ID"
+
 printf "TRAVIS_JOB_NUMBER\n"
 printenv "TRAVIS_JOB_NUMBER"
+
 printf "TRAVIS_PULL_REQUEST\n"
 printenv "TRAVIS_PULL_REQUEST"
+
 printf "TRAVIS_SECURE_ENV_VARS\n"
 printenv "TRAVIS_SECURE_ENV_VARS"
+
 printf "TRAVIS_REPO_SLUG\n"
 printenv "TRAVIS_REPO_SLUG"
+
 printf "TRAVIS_OS_NAME\n"
 printenv "TRAVIS_OS_NAME"
+
 printf "TRAVIS_TAG\n"
 printenv "TRAVIS_TAG"
+
 printf "TRAVIS_GO_VERSION\n"
 printenv "TRAVIS_GO_VERSION"
+
 printf "TRAVIS_JDK_VERSION\n"
 printenv "TRAVIS_JDK_VERSION"
+
 printf "TRAVIS_NODE_VERSION\n"
 printenv "TRAVIS_NODE_VERSION"
+
 printf "TRAVIS_OTP_RELEASE\n"
 printenv "TRAVIS_OTP_RELEASE"
+
 printf "TRAVIS_PERL_VERSION\n"
 printenv "TRAVIS_PERL_VERSION"
+
 printf "TRAVIS_PHP_VERSION\n"
 printenv "TRAVIS_PHP_VERSION"
+
 printf "TRAVIS_PYTHON_VERSION\n"
 printenv "TRAVIS_RUBY_VERSION"
+
 printf "TRAVIS_RUST_VERSION\n"
 printenv "TRAVIS_RUST_VERSION"
+
 printf "TRAVIS_SCALA_VERSION\n"
 printenv "TRAVIS_SCALA_VERSION"
+
 printf "TRAVIS_XCODE_SDK\n"
 printenv "TRAVIS_XCODE_SDK"
+
 printf "TRAVIS_XCODE_SCHEME\n"
 printenv "TRAVIS_XCODE_SCHEME"
+
 printf "TRAVIS_XCODE_PROJECT\n"
 printenv "TRAVIS_XCODE_PROJECT"
+
 printf "TRAVIS_XCODE_WORKSPACE\n"
 printenv "TRAVIS_XCODE_WORKSPACE"
 
