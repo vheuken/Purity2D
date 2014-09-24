75b8d9a46a57095af611c6d76c7651e46cf3f0ee
swloper <swloper@users.noreply.github.com>
2014-09-24 13:24:27 -0700

    Update travisvartest.sh

--- build-scripts/ci/travis/tests/travisvartest.sh
+++ build-scripts/ci/travis/tests/travisvartest.sh
@@ -8,35 +8,8 @@ printenv "TAVIS"
 printf "CONTINUOUS_INTEGRATION\n"
 printenv "CONTINUOUS_INTEGRATION"
 
-printf "DEBIAN_FRONTEND\n"
-printenv "DEBIAN_FRONTEND"
-
 printf "HAS_JOSH_K_SEAL_OF_APPROVAL\n"
 printenv "HAS_JOSH_K_SEAL_OF_APPROVAL"
-
-printf "USER - Travis note: do not depend on this value\n"
-printenv "USER"
-
-printf "HOME - Travis note: do not depend on this value\n"
-printenv "HOME"
-
-printf "LANG\n"
-printenv "LANG"
-
-printf "LC_ALL\n"
-printenv "LC_ALL"
-
-printf "RAILS_ENV\n"
-printenv "RAILS_ENV"
-
-printf "RACK_ENV\n"
-printenv "RACK_ENV"
-
-printf "MERB_ENV\n"
-printenv "MERB_ENV"
-
-printf "JRUBY_OPTS\n"
-printenv "JRUBY_OPTS"
 
 printf "TRAVIS_BRANCH\n"
 printenv "TRAVIS_BRANCH"
@@ -76,45 +49,6 @@ printenv "TRAVIS_OS_NAME"
 
 printf "TRAVIS_TAG\n"
 printenv "TRAVIS_TAG"
-
-printf "TRAVIS_GO_VERSION\n"
-printenv "TRAVIS_GO_VERSION"
-
-printf "TRAVIS_JDK_VERSION\n"
-printenv "TRAVIS_JDK_VERSION"
-
-printf "TRAVIS_NODE_VERSION\n"
-printenv "TRAVIS_NODE_VERSION"
-
-printf "TRAVIS_OTP_RELEASE\n"
-printenv "TRAVIS_OTP_RELEASE"
-
-printf "TRAVIS_PERL_VERSION\n"
-printenv "TRAVIS_PERL_VERSION"
-
-printf "TRAVIS_PHP_VERSION\n"
-printenv "TRAVIS_PHP_VERSION"
-
-printf "TRAVIS_PYTHON_VERSION\n"
-printenv "TRAVIS_RUBY_VERSION"
-
-printf "TRAVIS_RUST_VERSION\n"
-printenv "TRAVIS_RUST_VERSION"
-
-printf "TRAVIS_SCALA_VERSION\n"
-printenv "TRAVIS_SCALA_VERSION"
-
-printf "TRAVIS_XCODE_SDK\n"
-printenv "TRAVIS_XCODE_SDK"
-
-printf "TRAVIS_XCODE_SCHEME\n"
-printenv "TRAVIS_XCODE_SCHEME"
-
-printf "TRAVIS_XCODE_PROJECT\n"
-printenv "TRAVIS_XCODE_PROJECT"
-
-printf "TRAVIS_XCODE_WORKSPACE\n"
-printenv "TRAVIS_XCODE_WORKSPACE"
 
 
 
