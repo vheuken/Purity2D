2ce1d75a00ef8a84377d4f9db06fe7a7c5b5cfe9
swloper <swloper@users.noreply.github.com>
2014-09-24 11:57:46 -0700

    Update windows.ps1

--- build-scripts/windows.ps1
+++ build-scripts/windows.ps1
@@ -1,34 +1,32 @@
 #Requires -Version 3
 
-${Env:BUILD_OS} = "Windows"
-
 # Issue present on Appveyor
 Write-Host "Using workarounds for CMake errors"
 Rename-Item `
-         "C:\Program Files (x86)\Git\bin\sh.exe" `
-         "shworkaround"
+     "C:\Program Files (x86)\Git\bin\sh.exe" `
+     "shworkaround"
 Remove-Item `
-         -Recurse `
-         -Force `
-         "C:\MinGW"
+     -Recurse `
+     -Force `
+     "C:\MinGW"
 
 Write-Host "Installing MinGW"
 Invoke-WebRequest `
-         -Uri "http://iweb.dl.sourceforge.net/project/mingwbuilds/host-windows/releases/4.8.1/32-bit/threads-posix/sjlj/x32-4.8.1-release-posix-sjlj-rev5.7z" `
-         -Outfile x32-4.8.1-release-posix-sjlj-rev5.7z
+     -Uri "http://iweb.dl.sourceforge.net/project/mingwbuilds/host-windows/releases/4.8.1/32-bit/threads-posix/sjlj/x32-4.8.1-release-posix-sjlj-rev5.7z" `
+     -Outfile x32-4.8.1-release-posix-sjlj-rev5.7z
 7z.exe x `
-         -t7z `
-         "x32-4.8.1-release-posix-sjlj-rev5.7z" `
-         -o"${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\" `
-         | Find /V "Extracting " # 7z.exe does not have a quiet mode
+     -t7z `
+     "x32-4.8.1-release-posix-sjlj-rev5.7z" `
+     -o"${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\" `
+     | Find /V "Extracting " # 7z.exe does not have a quiet mode
 
 Write-Host "Adding MinGW to `${Env:Path}"
 ${Env:Path} = ${Env:Path} + ";${Env:ProgramFiles(x86)}\mingw-builds\x32-4.8.1-release-posix-sjlj-rev5\mingw32\bin"
 
 Write-Host "Creating `${DXSDK_DIR} at ${Env:ProgramFiles(x86)}\Microsoft DirectX SDK"
 Set-Variable `
-         -Name "DXSDK_DIR" `
-         -Value "${Env:ProgramFiles(x86)}\Microsoft DirectX SDK"
+     -Name "DXSDK_DIR" `
+     -Value "${Env:ProgramFiles(x86)}\Microsoft DirectX SDK"
 
 
 
@@ -37,28 +35,28 @@ ${Env:BUILD_HOME} = Get-Location
 
 Write-Host "Creating `${Env:BUILD_BIN} in ${Env:BUILD_HOME}\bin"
 New-Item "${Env:BUILD_HOME}\bin" `
-         -ItemType directory `
-         -Force `
-         ; ${Env:BUILD_BIN} = "${Env:BUILD_HOME}\bin"
+     -ItemType directory `
+     -Force `
+     ; ${Env:BUILD_BIN} = "${Env:BUILD_HOME}\bin"
 
 Write-Host "Creating `${Env:BUILD_ASSETS} in ${Env:BUILD_HOME}\assets"
 New-Item "${Env:BUILD_HOME}\assets" `
-         -ItemType directory `
-         -Force `
-         ; ${Env:BUILD_ASSETS} = "${Env:BUILD_HOME}\assets"
+     -ItemType directory `
+     -Force `
+     ; ${Env:BUILD_ASSETS} = "${Env:BUILD_HOME}\assets"
 
 Write-Host "Creating `${Env:BUILD_RELEASE} in ${Env:BUILD_HOME}\release"
 New-Item "${Env:BUILD_HOME}\release" `
-         -ItemType directory `
-         -Force `
-         ; ${Env:BUILD_RELEASE} = "${Env:BUILD_HOME}\release"
+     -ItemType directory `
+     -Force `
+     ; ${Env:BUILD_RELEASE} = "${Env:BUILD_HOME}\release"
 
 
 
 Write-Host "Building binaries"
 New-Item "${Env:BUILD_HOME}\build" `
-         -ItemType directory `
-         -Force
+     -ItemType directory `
+     -Force
 Set-Location "${Env:BUILD_HOME}\build"
 cmake.exe -G "MinGW Makefiles" ..
 mingw32-make.exe all
@@ -67,19 +65,19 @@ mingw32-make.exe all
 Write-Host "Building packages"
 Set-Location "${Env:BUILD_BIN}"
 New-Item ".\purity2d-build" `
-         -ItemType directory `
-         -Force
+     -ItemType directory `
+     -Force
 Copy-Item `
-         -Path ".\*.exe" `
-         -Destination ".\purity2d-build\"
+     -Path ".\*.exe" `
+     -Destination ".\purity2d-build\"
 Copy-Item `
-         -Path "${Env:BUILD_ASSETS}\*" `
-         -Destination ".\purity2d-build\" `
-         -Recurse
+     -Path "${Env:BUILD_ASSETS}\*" `
+     -Destination ".\purity2d-build\" `
+     -Recurse
 7z.exe a `
-         -tzip `
-         "purity2d-build.zip" `
-         .\purity2d-build
+     -tzip `
+     "purity2d-build.zip" `
+     .\purity2d-build
 
 
 Write-Host "Gathering final release files"
@@ -88,8 +86,8 @@ Write-Host "Contents of ${Env:BUILD_BIN}"
 Get-ChildItem
 Write-Host "Exporting release (ZIP) files"
 Copy-Item `
-         -Path ".\*.zip" `
-         -Destination "${Env:BUILD_RELEASE}\"
+     -Path ".\*.zip" `
+     -Destination "${Env:BUILD_RELEASE}\"
 
 
 Write-Host "Available releases:"
@@ -98,8 +96,8 @@ Get-ChildItem -Name
 
 Write-Host "Cleaning workaround for CMake errors"
 Rename-Item `
-         "C:\Program Files (x86)\Git\bin\shworkaround" `
-         "sh.exe"
+     "C:\Program Files (x86)\Git\bin\shworkaround" `
+     "sh.exe"
 
 
 
