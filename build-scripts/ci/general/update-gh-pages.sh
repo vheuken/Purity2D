3187213302423b254a96935ec0999829ef7f8b7e
swloper <swloper@users.noreply.github.com>
2014-09-24 12:37:58 -0700

    Update update-gh-pages.sh

--- build-scripts/ci/general/update-gh-pages.sh
+++ build-scripts/ci/general/update-gh-pages.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 # Requires Bash minimum version 3.1.0
 
+# Modified script originally from http://sleepycoders.blogspot.com/2013/03/sharing-travis-ci-generated-files.html
+
+# Currently makes the following assumptions:
+#     The CI environment has all the necessary CI variables set.
+#     The CI environment has the variable "${GH_TOKEN}" to connect to Github via OAuth token
+#     All uploaded files are located in "${CI_BUILD_DIRECTORY}/release/"
+
 
 if printf  "${CI_PULL_REQUEST_BOOLEAN_ALLOWED}" | grep -E "\"${CI_PULL_REQUEST_BOOLEAN}\"" > /dev/null; then
-    if [ "${CI_PULL_REQUEST_BOOLEAN}" == "true" ]; then
+     if [ "${CI_PULL_REQUEST_BOOLEAN}" == "true" ]; then
          printf "Pull request\n"
          exit 0
-    elif [ "${CI_PULL_REQUEST_BOOLEAN}" == "false" ]; then
+     elif [ "${CI_PULL_REQUEST_BOOLEAN}" == "false" ]; then
          printf "Starting gh-pages update\n"
          
          printf "Setting up Git\n"
@@ -37,13 +44,13 @@ if printf  "${CI_PULL_REQUEST_BOOLEAN_ALLOWED}" | grep -E "\"${CI_PULL_REQUEST_B
          
          printf "Done\n"
          exit 0
-    elif [ "${CI_PULL_REQUEST_BOOLEAN}" == "no data" ]; then
+     elif [ "${CI_PULL_REQUEST_BOOLEAN}" == "no data" ]; then
          printf "Warning: Unable to determine pull request status. Not pushing data.\n"
          exit 0
-    else
+     else
          printf "Error: Variable \${CI_PULL_REQUEST_BOOLEAN} returned \"${CI_PULL_REQUEST_BOOLEAN}\". Expected values: \"true\" \"false\" \"no data\". This script is unprepared to handle \"${CI_PULL_REQUEST_BOOLEAN}\" but has determined that \"${CI_PULL_REQUEST_BOOLEAN}\" is an allowed value.\n"
          exit 1
-    fi
+     fi
 elif env | grep -E "CI_PULL_REQUEST_BOOLEAN=" > /dev/null; then
      printf "Error: Variable \${CI_PULL_REQUEST_BOOLEAN} returned \"${CI_PULL_REQUEST_BOOLEAN}\". Expected values: ${CI_PULL_REQUEST_BOOLEAN_ALLOWED}.\n"
      exit 1
