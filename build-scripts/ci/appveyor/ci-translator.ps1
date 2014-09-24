6ab7df8cc3f3f598e0e0ad9db6ab4ef2f56aebea
swloper <swloper@users.noreply.github.com>
2014-09-23 22:35:55 -0700

    Update ci-translator.ps1

--- build-scripts/ci/appveyor/ci-translator.ps1
+++ build-scripts/ci/appveyor/ci-translator.ps1
@@ -18,8 +18,8 @@ else
 # CI_HOST
 if ( "${Env:APPVEYOR}" -eq "true" )
 {
-     Set-Variable `
-         -Name "CI_HOST" `
+     Set-Item `
+         -Path "Env:CI_HOST" `
          -Value "appveyor"
 }
 else
@@ -28,71 +28,71 @@ else
 }
 
 # 
-Set-Variable `
-     -Name "CI_API_URL" `
+Set-Item `
+     -Path "Env:CI_API_URL" `
      -Value "${Env:APPVEYOR_API_URL}"
 
 
 # 
-Set-Variable `
-     -Name "CI_PROJECT_ID" `
+Set-Item `
+     -Path "Env:CI_PROJECT_ID" `
      -Value "${Env:APPVEYOR_PROJECT_ID}"
 
 
 # 
-Set-Variable `
-     -Name "CI_PROJECT_NAME" `
+Set-Item `
+     -Path "Env:CI_PROJECT_NAME" `
      -Value "${Env:APPVEYOR_PROJECT_NAME}"
 
 
 # 
-Set-Variable `
-     -Name "CI_PROJECT_SLUG" `
+Set-Item `
+     -Path "Env:CI_PROJECT_SLUG" `
      -Value "${Env:APPVEYOR_PROJECT_SLUG}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_DIRECTORY" `
+Set-Item `
+     -Path "Env:CI_BUILD_DIRECTORY" `
      -Value "${Env:APPVEYOR_BUILD_FOLDER}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BIULD_ID" `
+Set-Item `
+     -Path "Env:CI_BIULD_ID" `
      -Value "${Env:APPVEYOR_BUILD_ID}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_NUMBER" `
+Set-Item `
+     -Path "Env:CI_BUILD_NUMBER" `
      -Value "${Env:APPVEYOR_BUILD_NUMBER}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_VERSION" `
+Set-Item `
+     -Path "Env:CI_BUILD_VERSION" `
      -Value "${Env:APPVEYOR_BUILD_VERSION}"
 
 
 # CI_PULL_REQUEST_BOOLEAN
 if ( "${Env:APPVEYOR_PULL_REQUEST_NUMBER}" -eq "" )
 {
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_BOOLEAN" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_BOOLEAN" `
          -Value "false"
 }
 elseif ( "${Env:APPVEYOR_PULL_REQUEST_NUMBER}" -match "(^\d+$)" )
 {
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_BOOLEAN" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_BOOLEAN" `
          -Value "true"
 }
 else
 {
      Write-Host "Unable to determine pull request status."
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_BOOLEAN" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_BOOLEAN" `
          -Value "no data"
 }
 
@@ -100,169 +100,169 @@ else
 # CI_PULL_REQUEST_NUMBER
 if ( "${Env:APPVEYOR_PULL_REQUEST_NUMBER}" -eq "" )
 {
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_NUMBER" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_NUMBER" `
          -Value "N/A"
 }
 elseif ( "${Env:APPVEYOR_PULL_REQUEST_NUMBER}" -match "(^\d+$)" )
 {
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_NUMBER" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_NUMBER" `
          -Value "${Env:APPVEYOR_PULL_REQUEST_NUMBER}"
 }
 else
 {
      Write-Host "Unable to determine pull request number."
-     Set-Variable `
-         -Name "CI_PULL_REQUEST_NUMBER" `
+     Set-Item `
+         -Path "Env:CI_PULL_REQUEST_NUMBER" `
          -Value "no data"
 }
 
 
 # 
-Set-Variable `
-     -Name "CI_PULL_REQUEST_TITLE" `
+Set-Item `
+     -Path "Env:CI_PULL_REQUEST_TITLE" `
      -Value "${Env:APPVEYOR_PULL_REQUEST_TITLE}"
 
 
 # 
-Set-Variable `
-     -Name "CI_JOB_ID" `
+Set-Item `
+     -Path "Env:CI_JOB_ID" `
      -Value "${Env:APPVEYOR_JOB_ID}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PROVIDER" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PROVIDER" `
      -Value "${Env:APPVEYOR_REPO_PROVIDER}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_SCM" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_SCM" `
      -Value "${Env:APPVEYOR_REPO_SCM}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_NAME" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_NAME" `
      -Value "${Env:APPVEYOR_REPO_NAME}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_BRANCH" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_BRANCH" `
      -Value "${Env:APPVEYOR_REPO_BRANCH}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_ID" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_ID" `
      -Value "${Env:APPVEYOR_REPO_COMMIT}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_AUTHOR" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_AUTHOR" `
      -Value "${Env:APPVEYOR_REPO_COMMIT_AUTHOR}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_EMAIL" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_EMAIL" `
      -Value "${Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_TIMESTAMP" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_TIMESTAMP" `
      -Value "${Env:APPVEYOR_REPO_COMMIT_TIMESTAMP}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_MESSAGE_SHORT" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_MESSAGE_SHORT" `
      -Value "${Env:APPVEYOR_REPO_COMMIT_MESSAGE}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PULL_MESSAGE_FULL" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PULL_MESSAGE_FULL" `
      -Value "${Env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_SCHEDULE_BOOLEAN" `
+Set-Item `
+     -Path "Env:CI_BUILD_SCHEDULE_BOOLEAN" `
      -Value "${Env:APPVEYOR_SCHEDULED_BUILD}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_COMPILER_PLATFORM" `
+Set-Item `
+     -Path "Env:CI_BUILD_COMPILER_PLATFORM" `
      -Value "${Env:PLATFORM}"
 
 
 # 
-Set-Variable `
-     -Name "CI_BUILD_COMPILER_CONFIGURATION" `
+Set-Item `
+     -Path "Env:CI_BUILD_COMPILER_CONFIGURATION" `
      -Value "${Env:CONFIGURATION}"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PUSH_AUTHOR" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PUSH_AUTHOR" `
      -Value "AppVeyor"
 
 
 # 
-Set-Variable `
-     -Name "CI_REPOSITORY_PUSH_EMAIL" `
+Set-Item `
+     -Path "Env:CI_REPOSITORY_PUSH_EMAIL" `
      -Value "appveyor@appveyor.com"
 
 
 # ${BUILD_OS} is supplied by the YAML file.
-Set-Variable `
-     -Name "CI_BUILD_OS" `
-     -Value "${BUILD_OS}"
+Set-Item `
+     -Path "Env:CI_BUILD_OS" `
+     -Value "${Env:BUILD_OS}"
 
 
 # Temporary variable
-Set-Variable `
-     -Name "CI_PULL_REQUEST_BOOLEAN_ALLOWED" `
+Set-Item `
+     -Path "Env:CI_PULL_REQUEST_BOOLEAN_ALLOWED" `
      -Value "${`"true`" `"false`" `"no data`"}"
 
 
-"${CI_HOST}"
-"${CI_API_URL}"
-"${CI_PROJECT_ID}"
-"${CI_PROJECT_NAME}"
-"${CI_PROJECT_SLUG}"
-"${CI_BUILD_DIRECTORY}"
-"${CI_BIULD_ID}"
-"${CI_BUILD_NUMBER}"
-"${CI_BUILD_VERSION}"
-"${CI_PULL_REQUEST_BOOLEAN}"
-"${CI_PULL_REQUEST_NUMBER}"
-"${CI_PULL_REQUEST_TITLE}"
-"${CI_JOB_ID}"
-"${CI_REPOSITORY_PROVIDER}"
-"${CI_REPOSITORY_SCM}"
-"${CI_REPOSITORY_NAME}"
-"${CI_REPOSITORY_BRANCH}"
-"${CI_REPOSITORY_PULL_ID}"
-"${CI_REPOSITORY_PULL_AUTHOR}"
-"${CI_REPOSITORY_PULL_EMAIL}"
-"${CI_REPOSITORY_PULL_TIMESTAMP}"
-"${CI_REPOSITORY_PULL_MESSAGE_SHORT}"
-"${CI_REPOSITORY_PULL_MESSAGE_FULL}"
-"${CI_BUILD_SCHEDULE_BOOLEAN}"
-"${CI_BUILD_COMPILER_PLATFORM}"
-"${CI_BUILD_COMPILER_CONFIGURATION}"
-"${CI_REPOSITORY_PUSH_AUTHOR}"
-"${CI_REPOSITORY_PUSH_EMAIL}"
-"${CI_BUILD_OS}"
-"${CI_PULL_REQUEST_BOOLEAN_ALLOWED}"
+"${Env:CI_HOST}"
+"${Env:CI_API_URL}"
+"${Env:CI_PROJECT_ID}"
+"${Env:CI_PROJECT_NAME}"
+"${Env:CI_PROJECT_SLUG}"
+"${Env:CI_BUILD_DIRECTORY}"
+"${Env:CI_BIULD_ID}"
+"${Env:CI_BUILD_NUMBER}"
+"${Env:CI_BUILD_VERSION}"
+"${Env:CI_PULL_REQUEST_BOOLEAN}"
+"${Env:CI_PULL_REQUEST_NUMBER}"
+"${Env:CI_PULL_REQUEST_TITLE}"
+"${Env:CI_JOB_ID}"
+"${Env:CI_REPOSITORY_PROVIDER}"
+"${Env:CI_REPOSITORY_SCM}"
+"${Env:CI_REPOSITORY_NAME}"
+"${Env:CI_REPOSITORY_BRANCH}"
+"${Env:CI_REPOSITORY_PULL_ID}"
+"${Env:CI_REPOSITORY_PULL_AUTHOR}"
+"${Env:CI_REPOSITORY_PULL_EMAIL}"
+"${Env:CI_REPOSITORY_PULL_TIMESTAMP}"
+"${Env:CI_REPOSITORY_PULL_MESSAGE_SHORT}"
+"${Env:CI_REPOSITORY_PULL_MESSAGE_FULL}"
+"${Env:CI_BUILD_SCHEDULE_BOOLEAN}"
+"${Env:CI_BUILD_COMPILER_PLATFORM}"
+"${Env:CI_BUILD_COMPILER_CONFIGURATION}"
+"${Env:CI_REPOSITORY_PUSH_AUTHOR}"
+"${Env:CI_REPOSITORY_PUSH_EMAIL}"
+"${Env:CI_BUILD_OS}"
+"${Env:CI_PULL_REQUEST_BOOLEAN_ALLOWED}"
 
 
 
