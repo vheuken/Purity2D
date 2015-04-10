da0c2afe9a29c2f234f1fefa955d36525fa56b84
Vincent Heuken <vincent@vincentheuken.com>
2015-04-09 22:25:06 -0700

    added Win32Window::Maximize fixes

--- src/framework/window/win32/window.cpp
+++ src/framework/window/win32/window.cpp
@@ -8,3 +8,39 @@ Purity::Win32Window::Win32Window(int width,
 {
 
 }
+
+void Purity::Win32Window::maximize()
+{
+    RECT workArea;
+    SystemParametersInfo(SPI_GETWORKAREA, 0, &workArea, 0);
+
+    SDL_SysWMinfo info;
+    SDL_GetWindowWMInfo(mInternalWindow, &info);
+    APPBARDATA appBarData;
+    appBarData.cbSize = sizeof(APPBARDATA);
+    appBarData.hWnd = info.info.win.window;
+    SHAppBarMessage(ABM_GETTASKBARPOS, &appBarData);
+
+    // bottom
+    if (appBarData.rc.top > appBarData.rc.left)
+    {
+        setPosition(Vector2i(0, 0));
+    }
+    //left
+    else if (appBarData.rc.top == appBarData.rc.left && appBarData.rc.bottom > appBarData.rc.right)
+    {
+        setPosition(Vector2i(appBarData.rc.right, 0));
+    }
+    // top
+    else if (appBarData.rc.top == appBarData.rc.left && appBarData.rc.bottom < appBarData.rc.right)
+    {
+        setPosition(Vector2i(0, appBarData.rc.bottom));
+    }
+    // right
+    else
+    {
+        setPosition(Vector2i(0, 0));
+    }
+
+    setSize(Vector2u(workArea.right - workArea.left, workArea.bottom - workArea.top));
+}
